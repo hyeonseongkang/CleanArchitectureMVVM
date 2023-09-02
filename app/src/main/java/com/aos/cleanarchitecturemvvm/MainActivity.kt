827c83c514e735f117bd4cc2feadcbaad4deb605
HyeonSeongKang <gt9697@naver.com>
2023-09-02 19:23:55 +0900

    chore: 코루틴 테스트 IO에서 suspend fun & async & await 사용해서 동시에 결과 받기

--- app/src/main/java/com/aos/cleanarchitecturemvvm/MainActivity.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/MainActivity.kt
@@ -2,11 +2,40 @@ package com.aos.cleanarchitecturemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    var TAG = "로그"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "${Thread.currentThread().name}1")
+
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d(TAG, "work.")
+            val network1 = async { network(1) }
+            val network2 = async {network(2) }
+            Log.d(TAG, network1.await() + " " + network2.await())
+        }
+        Log.d(TAG, "${Thread.currentThread().name}2")
+
+    }
+
+    fun hello() {
+        Log.d(TAG, "${Thread.currentThread().name} + hello!!")
+    }
+
+    suspend fun network(id: Int): String {
+        delay(2000)
+        return "network ${id}"
     }
 }
 
