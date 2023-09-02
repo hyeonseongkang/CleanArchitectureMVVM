9629d29503b9157263757403f0542835bd1fac59
HyeonSeongKang <gt9697@naver.com>
2023-09-02 19:26:01 +0900

    chore: 코루틴 공부 IO에서 네트워크 작업 후 결과 UI Thread로 보내기

--- app/src/main/java/com/aos/cleanarchitecturemvvm/MainActivity.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.aos.cleanarchitecturemvvm
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     var TAG = "로그"
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "${Thread.currentThread().name}1")
 
+        var textView = findViewById<TextView>(R.id.textView)
+
         CoroutineScope(Dispatchers.IO).launch {
             Log.d(TAG, "work.")
             val network1 = async { network(1) }
             val network2 = async {network(2) }
-            Log.d(TAG, network1.await() + " " + network2.await())
+            withContext(Dispatchers.Main) {
+                textView.text = network1.await() + " " + network2.await()
+            }
         }
         Log.d(TAG, "${Thread.currentThread().name}2")
 
