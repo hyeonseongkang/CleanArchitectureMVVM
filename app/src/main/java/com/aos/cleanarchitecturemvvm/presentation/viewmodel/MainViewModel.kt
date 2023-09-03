c0e7ff3a6b254a4431f6ce1b66108b3b7b295e98
HyeonSeongKang <gt9697@naver.com>
2023-09-03 22:44:17 +0900

    feat: MainViewModel 구현

--- app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/viewmodel/MainViewModel.kt
@@ -1,6 +1,16 @@
 package com.aos.cleanarchitecturemvvm.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.aos.cleanarchitecturemvvm.domain.model.User
+import com.aos.cleanarchitecturemvvm.domain.usecase.GetUserUseCase
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val getUserUseCase: GetUserUseCase) : ViewModel() {
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User> get() = _user
+
+    fun getUser(id: Int) {
+        _user.value = getUserUseCase.execute(id)
+    }
 }
