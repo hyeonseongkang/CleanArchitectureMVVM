2832547abaec2d583a78315d1a753b1356d36d2e
HyeonSeongKang <gt9697@naver.com>
2023-09-04 19:24:17 +0900

    feat: MainViewModel 업데이트

--- app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/viewmodel/MainViewModel.kt
@@ -5,12 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.aos.cleanarchitecturemvvm.domain.model.User
 import com.aos.cleanarchitecturemvvm.domain.usecase.GetUserUseCase
+import com.aos.cleanarchitecturemvvm.domain.usecase.UpdateUserEmailUseCase
 
-class MainViewModel(private val getUserUseCase: GetUserUseCase) : ViewModel() {
+class MainViewModel(private val getUserUseCase: GetUserUseCase,
+                    private val updateUserEmailUseCase: UpdateUserEmailUseCase
+) : ViewModel() {
     private val _user = MutableLiveData<User>()
     val user: LiveData<User> get() = _user
 
     fun getUser(id: Int) {
         _user.value = getUserUseCase.execute(id)
+    }
+
+    fun updateUserEmail(id: Int, email: String) {
+        _user.value = updateUserEmailUseCase.execute(id, email)
     }
 }
