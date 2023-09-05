9f2a3fa0cf22e6ccb89d8e02ffdf1c2c7b25df26
HyeonSeongKang <gt9697@naver.com>
2023-09-05 20:43:17 +0900

    feat: WritePostUseCase

---
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/domain/usecase/WritePostUseCase.kt
@@ -0,0 +1,7 @@
+package com.aos.cleanarchitecturemvvm.domain.usecase
+
+import com.aos.cleanarchitecturemvvm.domain.model.Post
+
+class WritePostUseCase (private val postRepository: PostRepository){
+    suspend fun execute(post: Post) = postRepository.writePost(post)
+}
