bf8c844bfd1fe8ab29f6bc7a5e94fecac2c99b24
HyeonSeongKang <gt9697@naver.com>
2023-09-07 19:56:58 +0900

    feat: LocalCommentDatSource

---
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/data/local/LocalCommentDataSource.kt
@@ -0,0 +1,17 @@
+package com.aos.cleanarchitecturemvvm.data.local
+
+import com.aos.cleanarchitecturemvvm.data.`interface`.CommentDataSource
+import com.aos.cleanarchitecturemvvm.domain.model.Comment
+
+class LocalCommentDataSource : CommentDataSource {
+    private val comments = mutableListOf<Comment>()
+
+    override suspend fun addComment(comment: Comment): Comment {
+        comments.add(comment)
+        return comment
+    }
+
+    override suspend fun getComments(postId: Int): List<Comment> {
+        return comments.filter { it.postId == postId }
+    }
+}
