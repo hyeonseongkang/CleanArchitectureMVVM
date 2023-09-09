bf3250df7b2edb7cc35e503d57e28335a7bf746e
HyeonSeongKang <gt9697@naver.com>
2023-09-09 17:38:06 +0900

    feat: PostDao

---
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/data/local/db/PostDao.kt
@@ -0,0 +1,16 @@
+package com.aos.cleanarchitecturemvvm.data.local.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.aos.cleanarchitecturemvvm.domain.model.Post
+
+@Dao
+interface PostDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPost(post: Post): Long
+
+    @Query("SELECT * FROM post")
+    suspend fun getAllPosts(): List<Post>
+}
