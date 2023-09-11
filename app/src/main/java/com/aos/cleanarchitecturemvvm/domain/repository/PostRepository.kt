54272c0920513ee9d91d028c239ffe35ed7f850b
HyeonSeongKang <gt9697@naver.com>
2023-09-11 20:39:50 +0900

    feat: PostRepository interface 추가

--- app/src/main/java/com/aos/cleanarchitecturemvvm/domain/repository/PostRepository.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/domain/repository/PostRepository.kt
@@ -4,7 +4,10 @@ import com.aos.cleanarchitecturemvvm.domain.model.Post
 
 interface PostRepository {
     suspend fun writePost(post: Post): Post
+
     suspend fun getPosts(): List<Post>
 
     suspend fun deletePost(post: Post): Int
+
+    suspend fun searchPost(query: String): List<Post>
 }
