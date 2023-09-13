c373ca5729a1c6a53075878b3e48faf82e299748
HyeonSeongKang <gt9697@naver.com>
2023-09-13 17:56:02 +0900

    chore: PostViewModelFactory 수정

--- app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/factory/PostViewModelFactory.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/factory/PostViewModelFactory.kt
@@ -2,16 +2,27 @@ package com.aos.cleanarchitecturemvvm.presentation.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.aos.cleanarchitecturemvvm.domain.usecase.DeletePostUseCase
 import com.aos.cleanarchitecturemvvm.domain.usecase.GetPostsUseCase
+import com.aos.cleanarchitecturemvvm.domain.usecase.SearchPostUseCase
+import com.aos.cleanarchitecturemvvm.domain.usecase.WritePostUseCase
 import com.aos.cleanarchitecturemvvm.presentation.viewmodel.PostViewModel
 
 class PostViewModelFactory(
-    private val getPostsUseCase: GetPostsUseCase
+    private val getPostsUseCase: GetPostsUseCase,
+    private val deletePostUseCase: DeletePostUseCase,
+    private val searchPostUseCase: SearchPostUseCase,
+    private val writePostUseCase: WritePostUseCase
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PostViewModel::class.java)) {
-            return PostViewModel(getPostsUseCase) as T
+            return PostViewModel(
+                getPostsUseCase,
+                deletePostUseCase,
+                searchPostUseCase,
+                writePostUseCase
+            ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
