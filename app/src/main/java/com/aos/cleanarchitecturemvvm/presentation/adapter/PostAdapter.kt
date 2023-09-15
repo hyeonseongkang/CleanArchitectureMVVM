1830cca82f3733269284bca77e4f74985e95907f
HyeonSeongKang <gt9697@naver.com>
2023-09-15 20:27:31 +0900

    feat: PostAdapter.kt

---
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/adapter/PostAdapter.kt
@@ -0,0 +1,37 @@
+package com.aos.cleanarchitecturemvvm.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aos.cleanarchitecturemvvm.databinding.ItemPostBinding
+import com.aos.cleanarchitecturemvvm.domain.model.Post
+
+class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+    private var posts: List<Post> = listOf()
+
+    fun setPosts(posts: List<Post>) {
+        this.posts = posts
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemPostBinding.inflate(inflater, parent, false)
+        return PostViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.bind(posts[position])
+    }
+
+    override fun getItemCount() = posts.size
+
+    class PostViewHolder(private val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(post: Post) {
+            binding.post = post
+            binding.executePendingBindings()
+        }
+    }
+}
