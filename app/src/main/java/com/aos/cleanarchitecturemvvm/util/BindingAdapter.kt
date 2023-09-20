7443fcef11e2012e73dd48d436cd627215d8015a
HyeonSeongKang <gt9697@naver.com>
2023-09-20 20:55:18 +0900

    feat: 검색 바인딩 어댑터 구현

--- app/src/main/java/com/aos/cleanarchitecturemvvm/util/BindingAdapter.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/util/BindingAdapter.kt
@@ -2,6 +2,7 @@ package com.aos.cleanarchitecturemvvm.util
 
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import androidx.databinding.BindingAdapter
 import com.aos.cleanarchitecturemvvm.R
 import com.aos.cleanarchitecturemvvm.presentation.viewmodel.PostViewModel
@@ -12,5 +13,13 @@ fun bindPostOnClick(button: Button, viewModel: PostViewModel) {
         val title = (button.rootView.findViewById(R.id.et_post_title) as EditText).text.toString()
         val content = (button.rootView.findViewById(R.id.et_post_content) as EditText).text.toString()
         viewModel.writePost(title, content)
+    }
+}
+
+@BindingAdapter("searchOnClick")
+fun bindSearchOnClick(button: ImageButton, viewModel: PostViewModel) {
+    button.setOnClickListener {
+        val query = (button.rootView.findViewById(R.id.et_search) as EditText).text.toString()
+        viewModel.searchPosts(query)
     }
 }
