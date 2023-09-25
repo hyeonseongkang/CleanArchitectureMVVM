7ccd17bfc7326f5d11765bb0548467a79d40165b
HyeonSeongKang <gt9697@naver.com>
2023-09-25 22:07:53 +0900

    feat: 갤러리 사진 가져와 imageView에 추가

--- app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/ui/CreatePostActivity.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/ui/CreatePostActivity.kt
@@ -1,11 +1,17 @@
 package com.aos.cleanarchitecturemvvm.presentation.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.databinding.DataBindingUtil
 import com.aos.cleanarchitecturemvvm.R
 import com.aos.cleanarchitecturemvvm.data.local.LocalPostDataSource
 import com.aos.cleanarchitecturemvvm.data.local.db.AppDatabase
 import com.aos.cleanarchitecturemvvm.data.repository.PostRepositoryImpl
+import com.aos.cleanarchitecturemvvm.databinding.ActivityCreatePostBinding
 import com.aos.cleanarchitecturemvvm.domain.usecase.DeletePostUseCase
 import com.aos.cleanarchitecturemvvm.domain.usecase.GetPostsUseCase
 import com.aos.cleanarchitecturemvvm.domain.usecase.SearchPostUseCase
@@ -16,6 +22,10 @@ import com.aos.cleanarchitecturemvvm.presentation.viewmodel.PostViewModel
 import com.aos.cleanarchitecturemvvm.util.getAppViewModelProvider
 
 class CreatePostActivity : AppCompatActivity() {
+
+    private val IMAGE_PICK_REQUEST_CODE = 1001
+
+    private lateinit var binding: ActivityCreatePostBinding
 
     private val viewModel by lazy {
         // Database 및 Dao 초기화
@@ -39,6 +49,44 @@ class CreatePostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_post)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_post)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        setupImageViewClickListener()
+    }
+
+    private fun setupImageViewClickListener() {
+        binding.ivPostPhoto.setOnClickListener {
+            pickImageFromGallery()
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_PICK_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            binding.ivPostPhoto.setImageURI(selectedImageUri)
+        }
+    }
+
+    private fun getPathFromURI(contentURI: Uri?): String? {
+        val result: String?
+        val cursor = contentResolver.query(contentURI!!, null, null, null, null)
+        if (cursor == null) {
+            result = contentURI.path
+        } else {
+            cursor.moveToFirst()
+            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+            result = cursor.getString(idx)
+            cursor.close()
+        }
+        return result
     }
 }
--- app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/presentation/ui/MainActivity.kt
@@ -28,8 +28,6 @@ import com.aos.cleanarchitecturemvvm.util.setupSwipeToDeleteAndEdit
 
 class MainActivity : AppCompatActivity() {
 
-    private val IMAGE_PICK_REQUEST_CODE = 1001
-
     private lateinit var binding: ActivityMainBinding
     private val viewModel by lazy {
         // Database 및 Dao 초기화
@@ -78,34 +76,4 @@ class MainActivity : AppCompatActivity() {
             postAdapter.setPosts(posts)
         })
     }
-
-    private fun pickImageFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == IMAGE_PICK_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
-            val selectedImageUri = data.data
-            val imagePath = getPathFromURI(selectedImageUri)
-            viewModel.writePost("Title", "Content", imagePath)
-        }
-    }
-
-    private fun getPathFromURI(contentURI: Uri?): String? {
-        val result: String?
-        val cursor = contentResolver.query(contentURI!!, null, null, null, null)
-        if (cursor == null) {
-            result = contentURI.path
-        } else {
-            cursor.moveToFirst()
-            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
-            result = cursor.getString(idx)
-            cursor.close()
-        }
-        return result
-    }
-
 }
