a1b4aac0fe648ba4ee240199dd59e2bdca4e0fa9
HyeonSeongKang <gt9697@naver.com>
2023-09-30 20:12:29 +0900

    chore: post xml 수정

--- app/src/main/java/com/aos/cleanarchitecturemvvm/util/BindingAdapter.kt
+++ app/src/main/java/com/aos/cleanarchitecturemvvm/util/BindingAdapter.kt
@@ -10,14 +10,14 @@ import com.aos.cleanarchitecturemvvm.R
 import com.aos.cleanarchitecturemvvm.presentation.viewmodel.PostViewModel
 import com.bumptech.glide.Glide
 
-@BindingAdapter("postOnClick")
-fun bindPostOnClick(button: Button, viewModel: PostViewModel) {
-    button.setOnClickListener {
-        val title = (button.rootView.findViewById(R.id.et_post_title) as EditText).text.toString()
-        val content = (button.rootView.findViewById(R.id.et_post_content) as EditText).text.toString()
-        viewModel.writePost(title, content)
-    }
-}
+//@BindingAdapter("postOnClick")
+//fun bindPostOnClick(button: Button, viewModel: PostViewModel) {
+//    button.setOnClickListener {
+//        val title = (button.rootView.findViewById(R.id.et_post_title) as EditText).text.toString()
+//        val content = (button.rootView.findViewById(R.id.et_post_content) as EditText).text.toString()
+//        viewModel.writePost(title, content)
+//    }
+//}
 
 @BindingAdapter("searchOnClick")
 fun bindSearchOnClick(button: ImageButton, viewModel: PostViewModel) {
